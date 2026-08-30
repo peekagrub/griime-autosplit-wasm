df26c27a659ff61c3742d8e21f8d89bcbdd0da73
peekagrub <25369813+peekagrub@users.noreply.github.com>
2026-08-30 21:43:40 +0200

    Remove loads ingame
    Main menu is currently untimed but has to become timed later

---
+++ src/lib.rs
@@ -0,0 +1,155 @@
+#![no_std]
+extern crate alloc;
+
+#[global_allocator]
+static ALLOC: dlmalloc::GlobalDlmalloc = dlmalloc::GlobalDlmalloc;
+
+mod memory;
+
+use alloc::format;
+use asr::{future::next_tick, print_message, timer::TimerState, Process};
+
+use crate::memory::Memory;
+
+asr::async_main!(stable);
+asr::panic_handler!();
+
+macro_rules! check_state_changed {
+    ($var:ident, $state:ident) => {
+        if $var != $state.$var {
+            print_message(&format!("{} = {}", stringify!($var), $var));
+            $state.$var = $var
+        }
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_and_check_state {
+    ($var:ident, $memory:ident, $class:ident, $inst_var:ident, $state:ident) => {
+        let $var = $memory.deref(&$memory.$class.$inst_var);
+
+        if $var.is_err() {
+            print_message(&format!(
+                "Couldn't read {}: {:?}",
+                stringify!($var),
+                $var.as_ref().unwrap_err()
+            ));
+        }
+
+        let $var = $var.unwrap_or_default();
+
+        check_state_changed!($var, $state);
+    };
+}
+
+struct AutosplitterState {
+    #[cfg(debug_assertions)]
+    streaming_paused: bool,
+    #[cfg(debug_assertions)]
+    last_teleport_time: f64,
+    #[cfg(debug_assertions)]
+    finish_teleport_time: f64,
+
+    #[cfg(debug_assertions)]
+    fade_out_time: f32,
+    #[cfg(debug_assertions)]
+    surrogate_active: bool,
+
+    #[cfg(debug_assertions)]
+    game_time_paused: bool,
+}
+
+impl AutosplitterState {
+    pub fn new() -> AutosplitterState {
+        AutosplitterState {
+            #[cfg(debug_assertions)]
+            streaming_paused: false,
+            #[cfg(debug_assertions)]
+            last_teleport_time: 0.0,
+            #[cfg(debug_assertions)]
+            finish_teleport_time: 0.0,
+
+            #[cfg(debug_assertions)]
+            fade_out_time: 0.0,
+            #[cfg(debug_assertions)]
+            surrogate_active: false,
+
+            #[cfg(debug_assertions)]
+            game_time_paused: false,
+        }
+    }
+}
+
+async fn main() {
+    asr::print_message("Hello, World!");
+
+    let mut state = AutosplitterState::new();
+
+    asr::print_message("main: AutosplitterState new...");
+
+    loop {
+        let process = Process::wait_attach("GRIME II.exe").await;
+
+        print_message(&format!(
+            "Found process path = {}",
+            process.get_path().unwrap_or_default()
+        ));
+        process
+            .until_closes(async {
+                print_message("main: until_closes...");
+                let memory = Memory::wait_attach(&process).await;
+                print_message("main: starting loop...");
+                loop {
+                    handle_loads(&mut state, &memory);
+                    next_tick().await;
+                }
+            })
+            .await;
+    }
+}
+
+fn handle_loads(state: &mut AutosplitterState, memory: &Memory) {
+    if asr::timer::state() != TimerState::Running {
+        return;
+    }
+
+    let streaming_paused: bool = memory
+        .deref(&memory.level_streaming.is_level_streaming_paused)
+        .unwrap_or_default();
+    let last_teleport_time: f64 = memory
+        .deref(&memory.level_streaming.last_teleport_time)
+        .unwrap_or_default();
+    let finish_teleport_time: f64 = memory
+        .deref(&memory.level_streaming.finish_teleport_time)
+        .unwrap_or_default();
+
+    let fade_out_time: f32 = memory
+        .deref(&memory.gui_menu.fade_out_time)
+        .unwrap_or_default();
+
+    let surrogate_active: bool = memory
+        .deref(&memory.checkpoint_menu.is_active)
+        .unwrap_or_default();
+
+    let game_time_paused = streaming_paused
+        || fade_out_time == -1.0
+        || (last_teleport_time > finish_teleport_time && !surrogate_active);
+    if game_time_paused {
+        asr::timer::pause_game_time();
+    } else {
+        asr::timer::resume_game_time();
+    }
+
+    #[cfg(debug_assertions)]
+    {
+        check_state_changed!(streaming_paused, state);
+        check_state_changed!(last_teleport_time, state);
+        check_state_changed!(finish_teleport_time, state);
+
+        check_state_changed!(fade_out_time, state);
+
+        check_state_changed!(surrogate_active, state);
+
+        check_state_changed!(game_time_paused, state);
+    }
+}
---
+++ src/memory.rs
@@ -0,0 +1,81 @@
+use asr::{
+    future::next_tick,
+    game_engine::unity::il2cpp::{Image, Module, UnityPointer},
+    Process,
+};
+use bytemuck::CheckedBitPattern;
+
+macro_rules! declare_pointers {
+    ($g:ident {$( $f:ident : $t:ty = $e:expr ),*,}) => {
+        pub struct $g {
+            $(pub $f: $t),*,
+        }
+
+        impl $g {
+            pub fn new() -> $g {
+                $g {
+                    $($f: $e),*,
+                }
+            }
+        }
+    };
+}
+
+declare_pointers!(LevelStreamingPointers {
+    is_level_streaming_paused: UnityPointer<2> = UnityPointer::new("LevelStreaming_PauseHandler", 0, &["instance", "isLevelStreamingPaused"]),
+    last_teleport_time: UnityPointer<2> = UnityPointer::new("LevelStreaming_Handler", 0, &["_instance", "lastTeleportTime"]),
+    finish_teleport_time: UnityPointer<2> = UnityPointer::new("LevelStreaming_Handler", 0, &["_instance", "finishTeleportTime"]),
+});
+
+declare_pointers!(GUIMenuPointers {
+    fade_out_time: UnityPointer<2> = UnityPointer::new("GUI_LoadingScreenHandler", 0, &["instance", "fadeOutTime"]),
+});
+
+declare_pointers!(GUICheckpointOptionsMenu {
+    is_active: UnityPointer<2> = UnityPointer::new("GUI_CheckpointOptionsMenu", 0, &["instance", "isActive"]),
+});
+
+pub struct Memory<'a> {
+    pub process: &'a Process,
+    pub module: Module,
+    pub image: Image,
+    pub level_streaming: LevelStreamingPointers,
+    pub gui_menu: GUIMenuPointers,
+    pub checkpoint_menu: GUICheckpointOptionsMenu,
+}
+
+impl Memory<'_> {
+    pub async fn wait_attach<'a>(process: &'a Process) -> Memory<'a> {
+        asr::print_message("Memory wait_attach: Module wait_attach...");
+        next_tick().await;
+
+        loop {
+            let module = Module::wait_attach_auto_detect(process).await;
+
+            asr::print_message("Memory wait_attach: Module get_image...");
+            next_tick().await;
+
+            for _ in 0..0x10 {
+                if let Some(image) = module.get_image(process, "AD_Scripts") {
+                    asr::print_message("Memory wait_attach: got module and image");
+                    return Memory {
+                        process,
+                        module,
+                        image,
+                        level_streaming: LevelStreamingPointers::new(),
+                        gui_menu: GUIMenuPointers::new(),
+                        checkpoint_menu: GUICheckpointOptionsMenu::new(),
+                    };
+                }
+                next_tick().await;
+            }
+        }
+    }
+
+    pub fn deref<T: CheckedBitPattern, const CAP: usize>(
+        &self,
+        p: &UnityPointer<CAP>,
+    ) -> Result<T, asr::Error> {
+        p.deref(self.process, &self.module, &self.image)
+    }
+}
