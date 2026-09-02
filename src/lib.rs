e316d26091e6e0da419ee775e3ab1def5e27b396
peekagrub <25369813+peekagrub@users.noreply.github.com>
2026-09-02 11:32:28 +0200

    Add was_loading_screen
    This makes it so that it no longer unpauses when quitting out to the Main Menu or when loading back into a save

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ static ALLOC: dlmalloc::GlobalDlmalloc = dlmalloc::GlobalDlmalloc;
 mod memory;
 
 use alloc::format;
-use asr::{future::next_tick, print_message, timer::TimerState, Process};
+use asr::{future::next_tick, print_message, timer::TimerState, Address64, Process};
 
 use crate::memory::Memory;
 
@@ -18,13 +18,30 @@ macro_rules! check_state_changed {
     ($var:ident, $state:ident) => {
         if $var != $state.$var {
             print_message(&format!("{} = {}", stringify!($var), $var));
-            $state.$var = $var
+        }
+    };
+
+    ($var:ident, $state:ident.$state_var:ident) => {
+        if $var != $state.$state_var {
+            print_message(&format!("{} = {}", stringify!($var), $var));
         }
     };
 }
 
+macro_rules! check_update_state_changed {
+    ($var:ident, $state:ident) => {
+        check_state_changed!($var, $state);
+        $state.$var = $var;
+    };
+
+    ($var:ident, $state:ident.$state_var:ident) => {
+        check_state_changed!($var, $state.$state_var);
+        $state.$state_var = $var;
+    };
+}
+
 #[allow(unused_macros)]
-macro_rules! read_and_check_state {
+macro_rules! read_and_check_update_state {
     ($var:ident, $memory:ident, $class:ident, $inst_var:ident, $state:ident) => {
         let $var = $memory.deref(&$memory.$class.$inst_var);
 
@@ -38,20 +55,21 @@ macro_rules! read_and_check_state {
 
         let $var = $var.unwrap_or_default();
 
-        check_state_changed!($var, $state);
+        check_update_state_changed!($var, $state);
     };
 }
 
 struct AutosplitterState {
+    was_loading_screen: bool,
+
     #[cfg(debug_assertions)]
     streaming_paused: bool,
     #[cfg(debug_assertions)]
-    last_teleport_time: f64,
-    #[cfg(debug_assertions)]
-    finish_teleport_time: f64,
+    is_teleporting: bool,
 
     #[cfg(debug_assertions)]
-    fade_out_time: f32,
+    is_faded_out: bool,
+
     #[cfg(debug_assertions)]
     surrogate_active: bool,
     #[cfg(debug_assertions)]
@@ -60,27 +78,34 @@ struct AutosplitterState {
     surrogate_allow_menu_close: bool,
 
     #[cfg(debug_assertions)]
+    player_unloaded: bool,
+
+    #[cfg(debug_assertions)]
     game_time_paused: bool,
 }
 
 impl AutosplitterState {
     pub fn new() -> AutosplitterState {
         AutosplitterState {
+            was_loading_screen: false,
+
             #[cfg(debug_assertions)]
             streaming_paused: false,
             #[cfg(debug_assertions)]
-            last_teleport_time: 0.0,
-            #[cfg(debug_assertions)]
-            finish_teleport_time: 0.0,
+            is_teleporting: false,
 
             #[cfg(debug_assertions)]
-            fade_out_time: 0.0,
+            is_faded_out: false,
+
             #[cfg(debug_assertions)]
             surrogate_active: false,
             #[cfg(debug_assertions)]
             surrogate_currently_open_panel: -1,
             #[cfg(debug_assertions)]
             surrogate_allow_menu_close: false,
+
+            #[cfg(debug_assertions)]
+            player_unloaded: false,
 
             #[cfg(debug_assertions)]
             game_time_paused: false,
@@ -124,16 +149,13 @@ fn handle_loads(state: &mut AutosplitterState, memory: &Memory) {
     let streaming_paused: bool = memory
         .deref(&memory.level_streaming.is_level_streaming_paused)
         .unwrap_or_default();
-    let last_teleport_time: f64 = memory
-        .deref(&memory.level_streaming.last_teleport_time)
-        .unwrap_or_default();
-    let finish_teleport_time: f64 = memory
-        .deref(&memory.level_streaming.finish_teleport_time)
+    let is_teleporting: bool = memory
+        .deref(&memory.level_streaming.is_teleporting)
         .unwrap_or_default();
 
-    let fade_out_time: f32 = memory
+    let is_faded_out: bool = memory
         .deref(&memory.gui_menu.fade_out_time)
-        .unwrap_or_default();
+        .is_ok_and(|t: f32| t == -1.0);
 
     let surrogate_active: bool = memory
         .deref(&memory.checkpoint_menu.is_active)
@@ -145,10 +167,16 @@ fn handle_loads(state: &mut AutosplitterState, memory: &Memory) {
         .deref(&memory.checkpoint_menu.allow_menu_close)
         .unwrap_or_default();
 
-    let game_time_paused = streaming_paused
-        || fade_out_time == -1.0
-        || (surrogate_currently_open_panel == -1 && !surrogate_allow_menu_close)
-        || (last_teleport_time > finish_teleport_time && !surrogate_active);
+    let player_unloaded: bool = memory
+        .deref(&memory.player_controller.instance)
+        .is_ok_and(|addr: Address64| addr.is_null());
+
+    let is_loading_screen =
+        is_faded_out && (is_teleporting || player_unloaded || state.was_loading_screen);
+
+    let game_time_paused = (streaming_paused && !player_unloaded)
+        || is_loading_screen
+        || (surrogate_currently_open_panel == -1 && !surrogate_allow_menu_close);
     if game_time_paused {
         asr::timer::pause_game_time();
     } else {
@@ -157,16 +185,21 @@ fn handle_loads(state: &mut AutosplitterState, memory: &Memory) {
 
     #[cfg(debug_assertions)]
     {
-        check_state_changed!(streaming_paused, state);
-        check_state_changed!(last_teleport_time, state);
-        check_state_changed!(finish_teleport_time, state);
+        check_state_changed!(is_loading_screen, state.was_loading_screen);
 
-        check_state_changed!(fade_out_time, state);
+        check_update_state_changed!(streaming_paused, state);
+        check_update_state_changed!(is_teleporting, state);
 
-        check_state_changed!(surrogate_active, state);
-        check_state_changed!(surrogate_currently_open_panel, state);
-        check_state_changed!(surrogate_allow_menu_close, state);
+        check_update_state_changed!(is_faded_out, state);
 
-        check_state_changed!(game_time_paused, state);
+        check_update_state_changed!(surrogate_active, state);
+        check_update_state_changed!(surrogate_currently_open_panel, state);
+        check_update_state_changed!(surrogate_allow_menu_close, state);
+
+        check_update_state_changed!(player_unloaded, state);
+
+        check_update_state_changed!(game_time_paused, state);
     }
+
+    state.was_loading_screen = is_loading_screen;
 }
--- src/memory.rs
+++ src/memory.rs
@@ -1,7 +1,8 @@
+use alloc::{string::String, vec::Vec};
 use asr::{
     future::next_tick,
     game_engine::unity::il2cpp::{Image, Module, UnityPointer},
-    Process,
+    Address64, Process,
 };
 use bytemuck::CheckedBitPattern;
 
@@ -23,8 +24,7 @@ macro_rules! declare_pointers {
 
 declare_pointers!(LevelStreamingPointers {
     is_level_streaming_paused: UnityPointer<2> = UnityPointer::new("LevelStreaming_PauseHandler", 0, &["instance", "isLevelStreamingPaused"]),
-    last_teleport_time: UnityPointer<2> = UnityPointer::new("LevelStreaming_Handler", 0, &["_instance", "lastTeleportTime"]),
-    finish_teleport_time: UnityPointer<2> = UnityPointer::new("LevelStreaming_Handler", 0, &["_instance", "finishTeleportTime"]),
+    is_teleporting: UnityPointer<2> = UnityPointer::new("LevelStreaming_Handler", 0, &["_instance", "isTeleportingToNewPosition"]),
 });
 
 declare_pointers!(GUIMenuPointers {
@@ -37,6 +37,10 @@ declare_pointers!(GUICheckpointOptionsMenu {
     allow_menu_close: UnityPointer<3> = UnityPointer::new("GUI_CheckpointOptionsMenu", 0, &["instance", "closeMenuButton", "m_Interactable"]),
 });
 
+declare_pointers!(PlayerController {
+    instance: UnityPointer<1> = UnityPointer::new("CharacterScript_Player_PlayerController", 0, &["instance"]),
+});
+
 pub struct Memory<'a> {
     pub process: &'a Process,
     pub module: Module,
@@ -44,6 +48,7 @@ pub struct Memory<'a> {
     pub level_streaming: LevelStreamingPointers,
     pub gui_menu: GUIMenuPointers,
     pub checkpoint_menu: GUICheckpointOptionsMenu,
+    pub player_controller: PlayerController,
 }
 
 impl Memory<'_> {
@@ -67,6 +72,7 @@ impl Memory<'_> {
                         level_streaming: LevelStreamingPointers::new(),
                         gui_menu: GUIMenuPointers::new(),
                         checkpoint_menu: GUICheckpointOptionsMenu::new(),
+                        player_controller: PlayerController::new(),
                     };
                 }
                 next_tick().await;
@@ -79,5 +85,18 @@ impl Memory<'_> {
         p: &UnityPointer<CAP>,
     ) -> Result<T, asr::Error> {
         p.deref(self.process, &self.module, &self.image)
+    }
+
+    #[allow(dead_code)]
+    pub fn read_string<const CAP: usize>(&self, p: &UnityPointer<CAP>) -> Option<String> {
+        let addr: Address64 = self.deref(p).ok()?;
+        let length: u32 = self.process.read(addr + 0x10).ok()?;
+
+        if length >= 2048 {
+            return None;
+        }
+
+        let wide_str: Vec<u16> = self.process.read_vec(addr + 0x14, length as usize).ok()?;
+        String::from_utf16(&wide_str).ok()
     }
 }
